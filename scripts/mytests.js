09f22fc358bbb1b9d8e62dfe64ba94dbbff7f9fc
Alexander <avburkush@gmail.com>
2022-05-26 19:51:24 +0300

    Added delete test buttons and confirmation modal

--- scripts/mytests.js
+++ scripts/mytests.js
@@ -17,16 +17,23 @@ const content = document.querySelector('.tests-list');
             const date = new Date(quiz.createdAt);
 
             content.innerHTML += `
-            <a class="test" href="./test.html?id=${quiz.id}">
-                <div>
-                    <img src="${quiz.author.avatar}" />
-                </div>  
-                <div>    
-                    <div>${quiz.author.name} ${quiz.author.surname}</div>
-                    <div class="test-date">${date.toLocaleTimeString()} ${date.getDate()}/${date.getMonth()}/${date.getFullYear()}</div>
-                    <div class="test-title">${quiz.name}</div>
+            <div class="test-container">
+                <div class="test-options">
+                    <button type="button" class="delete-item-btn" title="Видалити">
+                        <i class="fa-solid fa-trash-can"></i>
+                    </button>
                 </div>
-            </a>
+                <a class="test" href="./test.html?id=${quiz.id}">
+                    <div>
+                        <img src="${quiz.author.avatar}" />
+                    </div>  
+                    <div>    
+                        <div>${quiz.author.name} ${quiz.author.surname}</div>
+                        <div class="test-date">${date.toLocaleTimeString()} ${date.getDate()}/${date.getMonth()}/${date.getFullYear()}</div>
+                        <div class="test-title">${quiz.name}</div>
+                    </div>
+                </a>
+            </div>
             `;
         });
 
@@ -35,3 +42,68 @@ const content = document.querySelector('.tests-list');
     }
 })()
 .catch(err => console.log(err.message))
+
+// Delete tests
+
+const confirmDeleteModal = document.querySelector('.confirm-delete-modal');
+
+const closeConfirmBtn = document.querySelector('.close-confirm-modal');
+closeConfirmBtn.addEventListener('click', closeConfirmModal);
+
+const cancelBtn = document.querySelectorAll('.cancel-btn');
+for (let i = 0; i < cancelBtn.length; i++) {
+    cancelBtn[i].addEventListener('click', closeConfirmModal);
+}
+
+document.addEventListener('click', clickOutsideConfirm);
+document.addEventListener('keyup', escapeCloseConfirm);
+
+// По якому елементу був клік
+let item;
+
+function openConfirmModal(event) {
+    confirmDeleteModal.style.display = 'block';
+
+    item = event.target.parentElement.parentElement.nextElementSibling;
+    console.log(item);
+}
+
+function closeConfirmModal() {
+    confirmDeleteModal.style.display = 'none';
+}
+
+function clickOutsideConfirm (event) {
+    if(event.target === confirmDeleteModal) {
+        confirmDeleteModal.style.display = 'none';
+    }
+}
+
+function escapeCloseConfirm (event) {
+    if (event.keyCode === 27) {
+        confirmDeleteModal.style.display = 'none';
+    }
+}
+
+function getDeleteBtns() {
+    const openConfirmBtn = document.querySelectorAll('.delete-item-btn');
+
+    for (let i = 0; i < openConfirmBtn.length; i++) {
+        openConfirmBtn[i].addEventListener('click', openConfirmModal);
+    }
+}
+
+setTimeout(getDeleteBtns, 300);
+
+
+const deleteBtn = document.querySelectorAll('.yes-btn');
+for (let i = 0; i < deleteBtn.length; i++) {
+    deleteBtn[i].addEventListener('click', deleteTest);
+}
+
+function deleteTest() {
+    console.log('Deleted ' + item);
+
+    // ...
+
+    closeConfirmModal();
+}
