b900c1a796c130b7ccc55bbc018138d1b09169f8
Alexander <avburkush@gmail.com>
2022-05-28 22:19:17 +0300

    Small changes

--- scripts/myarticles.js
+++ scripts/myarticles.js
@@ -79,7 +79,7 @@ function escapeCloseConfirm (event) {
 }
 
 async function deleteArticle(id) {
-    const response = await fetch(`https://odbproject.herokuapp.com/api/articles/myarticles/${id}`, {
+    const response = await fetch(`https://odbproject.herokuapp.com/api/articles/${id}`, {
         method: "DELETE"
     });
 
